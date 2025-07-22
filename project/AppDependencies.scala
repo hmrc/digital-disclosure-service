8ff957fbcbb57579c4bc93ff37b1c7fa8e2039b5
Bon Parker <bonnie.parker@digital.hmrc.gov.uk>
2025-07-22 14:54:57 +0100

    DDCYLS-7075: Bootstrap 19.8.0 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.13.0"
+  private val bootstrapVersion = "9.18.0"
   private val pdfboxVersion = "2.0.26"
   private val openHtmlVersion = "1.0.10"
   private val playV = "play-30"
