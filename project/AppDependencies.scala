713e3d39cc7bdc82c964f3c275064b8a1a1ef4ca
vjwatson <171040641+VJWatson@users.noreply.github.com>
2026-02-13 15:52:42 +0000

    app deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.19.0"
+  private val bootstrapVersion = "10.5.0"
   private val pdfboxVersion = "2.0.26"
   private val openHtmlVersion = "1.0.10"
   private val playV = "play-30"
@@ -14,7 +14,7 @@ object AppDependencies {
     "uk.gov.hmrc"              %%  s"bootstrap-backend-$playV"     % bootstrapVersion,
     "uk.gov.hmrc"              %%  s"internal-auth-client-$playV"  % "4.3.0",
     "uk.gov.hmrc.mongo"        %%  s"hmrc-mongo-$playV"            % "2.12.0",
-    "uk.gov.hmrc"              %%  s"play-frontend-hmrc-$playV"    % "12.29.0",
+    "uk.gov.hmrc"              %%  s"play-frontend-hmrc-$playV"    % "12.31.0",
     "org.apache.pdfbox"        %   "pdfbox"                        % pdfboxVersion,
     "org.apache.pdfbox"        %   "xmpbox"                        % pdfboxVersion,
     "org.apache.xmlgraphics"   %   "batik-transcoder"              % "1.17",
